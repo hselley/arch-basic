9aa336ce53e8b95bc63a4cfde94fc46a40362125
Héctor Selley <hector.selley@gmail.com>
2023-07-26 12:56:10 -0600

    Agregué paquetes de Python
    * scikit
    * sympy
    * prettytable

--- 3.devel.sh
+++ 3.devel.sh
@@ -3,6 +3,8 @@
 # LaTeX
 sudo pacman -S texlive-fontsextra texlive-core texlive-bin texlive-bibtexextra texlive-formatsextra texlive-games texlive-humanities texlive-langcyrillic texlive-langextra texlive-langgreek texlive-latexextra texlive-music texlive-pictures texlive-pstricks texlive-publishers texlive-science texmaker gnome-latex kbibtex 
 
-# Science/Programming
-sudo pacman -S geogebra wine octave julia r gambas3-ide jupyterlab jupyter-notebook spyder code git xournalpp xf86-input-wacom pandoc asciidoctor
+# Programming
+sudo pacman -S geogebra wine octave julia r gambas3-ide jupyterlab jupyter-notebook code git xournalpp xf86-input-wacom pandoc asciidoctor
 
+# Science
+sudo pacman -S python-scikit-learn python-scikit-hep-testdata python-scikit-build python-prettytable python-sympy
