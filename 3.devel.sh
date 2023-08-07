0604ec7d3c6b5c44b105fdabb2d89993379e2482
Héctor Selley <hector.selley@gmail.com>
2023-08-07 12:39:34 -0600

    gcc-fortran
    Agregué gcc-fortran para compilar paquetes de R

--- 3.devel.sh
+++ 3.devel.sh
@@ -4,7 +4,7 @@
 sudo pacman -S texlive-fontsextra texlive-core texlive-bin texlive-bibtexextra texlive-formatsextra texlive-games texlive-humanities texlive-langcyrillic texlive-langextra texlive-langgreek texlive-latexextra texlive-music texlive-pictures texlive-pstricks texlive-publishers texlive-science texmaker gnome-latex kbibtex 
 
 # Programming
-sudo pacman -S geogebra wine octave julia r gambas3-ide jupyterlab jupyter-notebook code git xournalpp xf86-input-wacom pandoc asciidoctor
+sudo pacman -S geogebra wine octave julia r gambas3-ide jupyterlab jupyter-notebook code git xournalpp xf86-input-wacom pandoc asciidoctor gcc-fortran
 
 # Science
 sudo pacman -S python-scikit-learn python-scikit-hep-testdata python-scikit-build python-prettytable python-sympy
