50dfbb062d4aadcb69860f22d6188da74c40415d
Héctor Selley <hector.selley@gmail.com>
2026-07-29 14:22:23 -0600

    Paquetes adicionales de ghostty
    Agregué archivos complementarios de ghostty, agregan funcionalidad útil.

--- 2.software.sh
+++ 2.software.sh
@@ -17,7 +17,7 @@ sudo pacman -S qutebrowser gnome-podcasts geary chromium firefox-i18n-es-mx tele
 sudo pacman -S git filezilla htop fastfetch pacman-contrib reflector speedtest-cli
 sudo pacman -S cups cups-pdf inetutils dnsutils ntfs-3g
 sudo pacman -S bluez bluez-utils bluez-tools bluez-hid2hci
-sudo pacman -S alacarte alacritty ghostty ptyxis
+sudo pacman -S alacarte alacritty ghostty ghostty-terminfo ghostty-shell-integration ghostty-nautilus ptyxis
 sudo pacman -S power-profiles-daemon
 sudo pacman -S bitwarden btop nvtop obsidian libreoffice-fresh-es
 sudo pacman -S debugedit fakeroot flatpak 
